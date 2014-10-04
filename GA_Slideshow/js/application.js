d63c5d7b695ece4f5db85afa544206511adbf5a4
ZackDinerstein <zackdhome@gmail.com>
2014-10-04 16:43:28 -0700

    Assignment #5 - Slideshow - Feedback

--- GA_Slideshow/js/application.js
+++ GA_Slideshow/js/application.js
@@ -1,29 +1,28 @@
-var images = ["img/heart.jpg", "img/car.jpg", "img/hand.jpg", "img/tiger.jpg"]
+var images = ["img/car.jpg", "img/hand.jpg", "img/heart.jpg", "img/tiger.jpg"]; // Image srcs were out of order, which caused the wrong image to be displayed when a thumbnail was clicked. 
 var currentImage = 0;
 
 function changePicture(whichNum) {
- var img= document.getElementById('mainImage');
- img.setAttribute('src', images[whichNum]);
- currentImage = whichNum;
+	var img = document.getElementById('mainImage');
+	img.setAttribute('src', images[whichNum]);
+	currentImage = whichNum;
 }
 
-function nextImage (){
- currentImage++;
- if(currentImage===images.length){
- 	currentImage = 0;
- }
- var img = document.getElementById('mainImage');	
- img.setAttribute('src', images[currentImage]);
+function nextImage(){
+	currentImage++;
+	if (currentImage === images.length){
+		currentImage = 0;
+	}
+	var img = document.getElementById('mainImage');	
+	img.setAttribute('src', images[currentImage]);
 }
 
-
-function prevImage ( ) {
-currentImage= currentImage -1;
- if(currentImage===-1){
- currentImage =0;
-}
-var img= document.getElementById('mainImage');	
-img.setAttribute('src', images[currentImage]);
+function prevImage() {
+	currentImage = currentImage -1;
+		if (currentImage == -1){
+		currentImage = images.length -1; // This line is saying, if we're at image 0 and we're trying to go to an image before that one, set currentImage to the lenght of our 'images' array -1. Since the lenght of our images array is 4, we'll be setting 'current image' to 3, which is the index of the last image in our array. By writing our code like this (i.e. not hard-coding in a value like currentImage = 3), we can add as many images to our array as we want. Then, when we're on image 0 and we hit the previous button, we'll allways be setting 'currentImage' to the index of the last item in our array. Does that make sense? 
+	}
+	var img = document.getElementById('mainImage');	
+	img.setAttribute('src', images[currentImage]);
 }
 
 //function forwardPic(){
